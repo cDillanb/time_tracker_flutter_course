3e5f5bce7d22d1782b32bee2e856fc890554d031
Chad B <bounouscade99@gmail.com>
2022-04-13 14:03:23 -0600

    implemented streams for auth

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key, @required required this.auth, @required required this.onSignOut}) : super(key: key);
+  final VoidCallback onSignOut;
+  final AuthBase auth;
+
+  Future<void> _signOut() async {
+    try {
+      await auth.signOut();
+      onSignOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +23,9 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
         actions: <Widget>[
           TextButton(
-            onPressed: () {},
-            child: Text('Logout',
+            onPressed: _signOut,
+            child: Text(
+              'Logout',
               style: TextStyle(
                   fontSize: 18.0,
                   color: Colors.white),
--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'sign_in/sign_in_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'home_page.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key,@required required this.auth}) : super(key: key);
+
+  final AuthBase auth;
   @override
   State<LandingPage> createState() => _LandingPageState();
 }
@@ -12,20 +16,44 @@ class _LandingPageState extends State<LandingPage> {
 
   User? _user;
 
-  void _updateUser(User user) {
+  @override
+  void initState() {
+    super.initState();
+    _updateUser(widget.auth.currentUser);
+  }
+
+  void _updateUser(User? user) {
     setState(() {
       _user = user;
     });
-    print('User id ${user.uid}');
+    print('User id ${user?.uid}');
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_user == null) {
-      return SignInPage(
-        onSignIn: _updateUser,
-      );
-    }
-    return HomePage();
+    return StreamBuilder<User?>(
+      stream: widget.auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final User? user = snapshot.data;
+          if (user == null) {
+            return SignInPage(
+              auth: widget.auth,
+              onSignIn: _updateUser,
+            );
+          }
+          return HomePage(
+            auth: widget.auth,
+            onSignOut: () => _updateUser(null),
+          );
+        }
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          )
+        );
+      }
+    );
+
   }
 }
